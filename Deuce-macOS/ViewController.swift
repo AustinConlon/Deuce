8c9bb1769a07d1d903259320f2a984e909bfc262
Austin Conlon <austinconlon@icloud.com>
2019-03-09 23:47:19 -0800

    start macOS UI

---
+++ Deuce-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Deuce-macOS
+//
+//  Created by Austin Conlon on 3/3/19.
+//  Copyright © 2019 Austin Conlon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
