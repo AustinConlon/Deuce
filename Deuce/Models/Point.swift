601ed1ac1df59d52fa74295be092dd9835b77dad
Austin Conlon <austinconlon@icloud.com>
2020-07-02 23:58:41 -0700

    Begin factoring each point out to its own structure

---
+++ Deuce/Models/Point.swift
@@ -0,0 +1,13 @@
+//
+//  Point.swift
+//  Deuce
+//
+//  Created by Austin Conlon on 7/2/20.
+//  Copyright © 2020 Austin Conlon. All rights reserved.
+//
+
+import Foundation
+
+struct Point {
+    var score = [0, 0]
+}
